abf0e34cd6b8a73704310fa6cf881396c242e34f
Kristofer Lindberg <k.lindberg@gmail.com>
2015-04-27 20:29:31 +0200

    if we do not have the colors, we cannot solve it

--- test_puzzle_solver.lua
+++ test_puzzle_solver.lua
@@ -4,8 +4,16 @@ local ps = require("puzzle_solver")
 
 function test_puzzle_solver.test_solvable()
     local player = {r=255, g=255, b=255}
+    -- the 1x5 should be solvable too but we need to be able to walk
+    -- back and forth too then
     local result = ps.solvable({"@", 2, 1, 1, 1, "e"}, 6, player)
     return t.assert_equal(true, result)
+end
+
+function test_puzzle_solver.test_unsolvable()
+    local player = {r=255, g=255, b=255}
+    local result = ps.solvable({"@", 2, 2, 2, "e"}, 5, player)
+    return t.assert_equal(false, result)
 end
 
 function test_puzzle_solver.test_empty_list_not_solvable()
